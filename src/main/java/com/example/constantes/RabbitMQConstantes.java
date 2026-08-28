d8f1a15ad82671c8579128a247ff8e10adbb2335
Lucas Vanzella <lucasvanzellap2@gmail.com>
2026-08-27 23:43:31 -0300

    DLQs

--- src/main/java/com/example/constantes/RabbitMQConstantes.java
+++ src/main/java/com/example/constantes/RabbitMQConstantes.java
@@ -4,5 +4,6 @@ public class RabbitMQConstantes {
     public static final String FILA_ESTOQUE = "ESTOQUE";
     public static final String FILA_PRECO = "PRECO";
     
+    public static final String ESTOQUE_DLQ = "ESTOQUE.DLQ";
     
 }
